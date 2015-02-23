1c6f86c8bd9e9a8b357fd82093746583aa2aae7d
Shaina Jordan <sj237@mac.com>
2015-02-22 19:46:02 -0700

    more steps added to logistic_regression

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pandas as pd
 import statsmodels.api as sm
+import matplotlib.pyplot as plt
+import matplotlib.mlab as mlab
 
 loansData = pd.read_csv('https://spark-public.s3.amazonaws.com/dataanalysis/loansData.csv')
 loansData.to_csv('loansData_clean.csv', header=True, index=False)
@@ -13,19 +15,41 @@ loansData['FICO.Score'] = loansData['FICO.Range'].map(lambda x: int(x.split('-')
 loansData['intRateEval'] = map(lambda x: x < .12, loansData['Interest.Rate'])
 #loansData['intRateEval'] = loansData['Interest.Rate'] <=.12
 intRateEval = loansData['intRateEval']
-print intRateEval
 
 #create intercept column with value 1
 loansData['Intercept'] = 1
-
-#dependent variable
-y = np.matrix(intRateEval).transpose()
 
 loanamt = loansData['Amount.Requested']
 fico = loansData['FICO.Score']
 intercept = loansData['Intercept']
 
 ind_vars = loansData[['Intercept', 'FICO.Score', 'Amount.Requested']]
-print ind_vars
 
-logit = sm.Logit(loansData['intRateEval'], loansData[ind_vars])
+logit = sm.Logit(loansData['intRateEval'], ind_vars)
+result = logit.fit()
+
+coeff = result.params
+print coeff
+
+#x = np.arange(-2,16)
+#plt.plot(x, 1/(1+np.exp(-x)))
+
+#plt.show()
+
+#def logistic_function(intercept, fico, loanamt, coeff):
+#	p = 1/(1 + np.exp(intercept + coeff*(fico) - (loanamt)))
+#	print p
+
+def logistic_function(intercept, fico, loanamt):
+	p = 1/(1 + np.exp(-60.125045 + .087423*(fico) - .000174*(loanamt)))
+	print p
+
+fico = 720
+loanamt = 10000
+
+logistic_function(intercept, fico, loanamt)
+
+
+
+
+	
